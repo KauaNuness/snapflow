49fafea3bf7dcd01187b563b8a58810d9c8feb07
kauanuness <kaua.souza@sptech.school>
2025-02-03 22:30:43 -0300

    config dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
+require("dotenv").config
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
-const port = 5000;
+const port = process.env.PORT;
 const app = express();
 
-// Configurações JSON e FORM DATA RESPONSE
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
