e2d4f04a176224eb764c94d04dad394ea9d469bc
kauanuness <kaua.souza@sptech.school>
2025-02-14 20:29:17 -0300

    atualização de foto

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -1,6 +1,7 @@
 const Photo = require("../models/Photo");
 const mongoose = require("mongoose");
 const User = require("../models/User");
+const { update } = require("./UserController");
 
 const insertPhoto = async(req, res) => {
     const {title} = req.body;
@@ -61,8 +62,68 @@ const deletePhoto = async (req, res) => {
     }
 };
 
+const getAllPhotos = async (req, res) => {
+    const photos = await Photo.find({}).sort([["createdAt", -1]]).exec();
+
+    return res.status(200).json(photos);
+}
+
+const getUserPhotos = async(req, res) => {
+    const {id} = req.params;
+
+    const photos = await Photo.find({ userId: id}).sort([["createdAt", -1]]).exec();
+
+    return res.status(200).json(photos);
+}
+
+const getPhotoById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const photo = await Photo.findById(new mongoose.Types.ObjectId(id));
+
+        if (!photo) {
+            return res.status(404).json({ errors: ["Foto não encontrada"] });
+        }
+
+        res.status(200).json(photo);
+    } catch (error) {
+        res.status(400).json({ errors: ["ID inválido"] });
+    }
+};
+
+const updatePhoto = async(req, res) => {
+    const {id} = req.params;
+    const {title} = req.body;
+
+    const reqUser = req.user;
+
+    const photo = await Photo.findById(id);
+
+    if(!photo) {
+        res.status(404).json({errors: ["Foto não encontrada"]})
+        return
+    }
+
+    if(!photo.userId.equals(reqUser._id)){
+        res.status(422).json({errors: ["Ocorreu um erro, tente novamente"]})
+        return;
+    }
+
+    if(title) {
+        photo.title = title;
+    }
+
+    await photo.save();
+
+    res.status(200).json({photo, message: "Foto atualizado com sucesso"})
+}
 
 module.exports = {
     insertPhoto,
-    deletePhoto
+    deletePhoto,
+    getAllPhotos,
+    getUserPhotos,
+    getPhotoById,
+    updatePhoto,
 }
--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const {insertPhoto, deletePhoto} = require("../controllers/PhotoController");
+const {insertPhoto, deletePhoto, getAllPhotos, getUserPhotos, getPhotoById, updatePhoto} = require("../controllers/PhotoController");
 
-const {photoInsertValidation} = require("../middlewares/photoValidation");
+const {photoInsertValidation, photoUpdateValidation} = require("../middlewares/photoValidation");
 const authGuard = require("../middlewares/authGuard");
 const validate = require("../middlewares/handleValidation");
 const {imageUpload} = require("../middlewares/imageUpload");
 
 router.post("/", authGuard, imageUpload.single("image"), photoInsertValidation(), validate, insertPhoto);
-router.delete("/:id", authGuard, deletePhoto)
+router.delete("/:id", authGuard, deletePhoto);
+router.get("/", authGuard, getAllPhotos);
+router.get("/user/:id", authGuard, getUserPhotos);
+router.get("/:id", authGuard, getPhotoById);
+router.put("/:id", authGuard, photoUpdateValidation(), validate,updatePhoto);
 
 module.exports = router;
